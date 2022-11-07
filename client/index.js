df2be93a65a2869af8ce95b279e764ec5e542e3e
cesar <transformers.cesar115@gmail.com>
2022-11-07 08:16:30 -0600

    instalando materialize

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 // Cargado estilos
-import './stylesheet/myStyles.css';
-import './stylesheet/style.css';
+// import './stylesheet/myStyles.css';
+// import './stylesheet/style.css';
+// importando materialize
 
+import 'materialize-css/dist/css/materialize.min.css';
+// importando scrips de materialize
+import 'materialize-css/dist/js/materialize';
+
+// inicializando los scrips
+M.AutoInit();
 console.log('Webpack Working Middleware!!! 📦');
